24e1136fda13e87f2d8c26d5b4f321afd9ff67f9
JJY <kaslock@naver.com>
2020-05-18 18:59:26 +0900

    Location Search and Guidance

--- Biking/app/src/main/java/cau/injiyong/biking/ui/send/SendFragment.java
+++ Biking/app/src/main/java/cau/injiyong/biking/ui/send/SendFragment.java
@@ -120,13 +120,17 @@
 
 package cau.injiyong.biking.ui.send;
 
+import android.Manifest;
 import android.app.AlertDialog;
 import android.content.DialogInterface;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
+import android.graphics.PointF;
 import android.location.Location;
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -138,12 +142,15 @@ import android.widget.RelativeLayout;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import cau.injiyong.biking.MainActivity;
 import cau.injiyong.biking.R;
 import noman.googleplaces.PlaceType;
 
 import com.skt.Tmap.TMapData;
 import com.skt.Tmap.TMapGpsManager;
+import com.skt.Tmap.TMapMarkerItem;
 import com.skt.Tmap.TMapPOIItem;
 import com.skt.Tmap.TMapPoint;
 import com.skt.Tmap.TMapPolyLine;
@@ -152,13 +159,20 @@ import com.skt.Tmap.TMapView;
 import java.util.ArrayList;
 import java.util.logging.LogManager;
 
+import static cau.injiyong.biking.ui.home.HomeFragment.location;
+
 
 public class SendFragment extends Fragment implements TMapGpsManager.onLocationChangedCallback {
+
+    TMapPoint Current_Point;
+    double getCurrent_long;
+    double getCurrent_lat;
 
     private TMapView tmapview;
     private TMapGpsManager tmapgps = null;
     TMapPoint Destination_Point = null;
     private String Address;
+    private TMapMarkerItem CurrentMarker;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -167,17 +181,32 @@ public class SendFragment extends Fragment implements TMapGpsManager.onLocationC
 
         tmapview = (TMapView)rootView.findViewById(R.id.tmapmap);
 
-
         tmapview.setSKTMapApiKey("l7xx3ce387d7e7764c70ba53c4cddb6391eb");
         tmapview.setLanguage(TMapView.LANGUAGE_KOREAN);
 
-        tmapview.setLocationPoint(126.9584046, 37.5018292);
+        tmapview.setLocationPoint(location.getLongitude(), location.getLatitude());
 
-        tmapview.setIconVisibility(true);
+        setMapIcon();
         tmapview.setZoomLevel(14);
         tmapview.setMapType(TMapView.MAPTYPE_STANDARD);
         tmapview.setCompassMode(true);
         tmapview.setTrackingMode(true);
+
+        // Gps Open
+        tmapgps = new TMapGpsManager(getContext());
+        tmapgps.setMinTime(1000);
+        tmapgps.setMinDistance(2);
+        tmapgps.setProvider(tmapgps.NETWORK_PROVIDER);
+        if(ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)!=PackageManager.PERMISSION_GRANTED){
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                requestPermissions(new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1); //위치권한 탐색 허용 관련 내용
+            }
+            return rootView;
+        }
+        tmapgps.setProvider(tmapgps.GPS_PROVIDER);
+        tmapgps.OpenGps();
 
 
         Button button_Demo = (Button) rootView.findViewById(R.id.btn_Demo);
@@ -204,25 +233,34 @@ public class SendFragment extends Fragment implements TMapGpsManager.onLocationC
                 tmapview.setTMapPathIcon(start, end);
                 tmapview.zoomToTMapPoint(point1, point2);
 
-                tmapgps = new TMapGpsManager(getContext());
-                tmapgps.setMinTime(1000);
-                tmapgps.setMinDistance(5);
-                tmapgps.setProvider(tmapgps.NETWORK_PROVIDER);
-                tmapgps.setProvider(tmapgps.GPS_PROVIDER);
-                tmapgps.OpenGps();
                 tmapview.setTrackingMode(true);
                 tmapview.setSightVisible(true);
 
             }
         });
 
-        Button button_convenienceStore = (Button) rootView.findViewById(R.id.btn_convenienceStore);
-        button_convenienceStore.setOnClickListener(new View.OnClickListener() {
+        Button button_search = (Button) rootView.findViewById(R.id.btn_search);
+        button_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TMapPoint tpoint = new TMapPoint(37.5018292, 126.9584046); // 집
                 SearchDestination();
 
+            }
+        });
+
+        Button button_select = (Button) rootView.findViewById(R.id.btn_select);
+        button_select.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ClickDestination();
+            }
+        });
+
+        Button button_start = (Button) rootView.findViewById(R.id.btn_start);
+        button_start.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StartGuidance();
             }
         });
 
@@ -232,6 +270,12 @@ public class SendFragment extends Fragment implements TMapGpsManager.onLocationC
     @Override
     public void onLocationChange(Location location) {
         tmapview.setLocationPoint(location.getLongitude(),location.getLatitude());
+        tmapview.setCenterPoint(location.getLongitude(), location.getLatitude());
+
+        getCurrent_long = location.getLongitude();
+        getCurrent_lat = location.getLatitude();
+
+        Current_Point = new TMapPoint(getCurrent_lat, getCurrent_long);
 
     }
 
@@ -275,6 +319,72 @@ public class SendFragment extends Fragment implements TMapGpsManager.onLocationC
         });
         builder.show();
     }
+
+    public void ClickDestination() {
+        Toast.makeText(getContext(), "원하시는 도착 지점을 터치한 후 길안내 시작버튼을 눌러주세요.", Toast.LENGTH_SHORT).show();
+
+        tmapview.setOnClickListenerCallBack(new TMapView.OnClickListenerCallback() {
+            @Override
+            public boolean onPressEvent(ArrayList<TMapMarkerItem> arrayList,
+                                        ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
+
+                TMapData tMapData = new TMapData();
+                tMapData.convertGpsToAddress(tMapPoint.getLatitude(), tMapPoint.getLongitude(),
+                        new TMapData.ConvertGPSToAddressListenerCallback() {
+                            @Override
+                            public void onConvertToGPSToAddress(String strAddress) {
+                                Address = strAddress;
+                            }
+                        });
+
+                Toast.makeText(getContext(), "선택하신 위치의 주소는 " + Address + " 입니다.", Toast.LENGTH_SHORT).show();
+
+                return false;
+            }
+
+            @Override
+            public boolean onPressUpEvent(ArrayList<TMapMarkerItem> arrayList,
+                                          ArrayList<TMapPOIItem> arrayList1, TMapPoint tMapPoint, PointF pointF) {
+                Destination_Point = tMapPoint;
+
+                return false;
+            }
+        });
+
+    }
+
+    public void StartGuidance() {
+        tmapview.removeTMapPath();
+
+        TMapPoint point1 = tmapview.getLocationPoint();
+        TMapPoint point2 = Destination_Point;
+
+        TMapData tmapdata = new TMapData();
+
+        tmapdata.findPathDataWithType(TMapData.TMapPathType.CAR_PATH, point1, point2, new TMapData.FindPathDataListenerCallback() {
+            @Override
+            public void onFindPathData(TMapPolyLine polyLine) {
+                polyLine.setLineColor(Color.BLUE);
+                tmapview.addTMapPath(polyLine);
+            }
+        });
+
+        Bitmap start = BitmapFactory.decodeResource(getContext().getResources(),R.drawable.poi_start);
+        Bitmap end = BitmapFactory.decodeResource(getContext().getResources(),R.drawable.poi_end);
+        tmapview.setTMapPathIcon(start, end);
+        tmapview.zoomToTMapPoint(point1, point2);
+    }
+
+    /* 현재위치로 표시될 아이콘을 설정한다. */
+    public void setMapIcon() {
+        CurrentMarker = new TMapMarkerItem();
+
+        Bitmap bitmap = BitmapFactory.decodeResource(getContext().getResources(), R.drawable.poi_here);
+        tmapview.setIcon(bitmap);
+        tmapview.addMarkerItem("CurrentMarker", CurrentMarker);
+        tmapview.setIconVisibility(true);
+    }
+
 
 
 }
